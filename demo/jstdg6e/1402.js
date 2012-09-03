d2c2560b9589d1e9d44c6309b55a030d174300a8
wkliang <wkliang@3-link.com.tw>
2012-09-03 16:39:02 +0800

    1402.js

---
+++ demo/jstdg6e/1402.js
@@ -0,0 +1,26 @@
+
+// pg#344
+// Example 14-2. Extracting arguments from the search string of a URL
+/*
+ * This function parses ampersand-separated name=value argument pairs from
+ * the query string of the URL. It stores the name=value pairs in 
+ * properties of an object and returns that object. Us it like this:
+ *
+ * var args = urlArgs();	// Parse args from URL
+ * var q = args.q || "";	// Use argument, if defined, or a default value
+ * var n = args.n ? parsInt(args.n) : 10;
+ */
+function urlArgs() {
+    var args = {};				// Start with an empty object
+    var query = location.search.substring(1);	// Get query string, minus '?'
+    var pairs = query.split("&");		// Split at ampersands
+    for (var i = 0; i < pairs.length; i++) {	// For each fragment
+	var pos = pairs[i].indexOf('=');	// Look for "name=value"
+	if (pos == -1 ) continue;		// If not found, skip it
+	var name = pairs[i].substring(0,pos);	// Extract the name
+	var value = pairs[i].substring(pos+1);	// Extract the value
+	value = decodeURIComponent(value);	// Decode the value
+	args[name = value;			// Store as a property
+    }
+    return args;				// Return the parsed arguments
+}
