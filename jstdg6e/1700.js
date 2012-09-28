8164b8553a1aa3d40e0a417d0e2d69567acd50b7
wkliang <wenkui.liang@gmail.com>
2012-09-28 16:40:23 +0800

    before chapter 20

--- jstdg6e/1700.js
+++ jstdg6e/1700.js
@@ -32,9 +32,13 @@ function cancelHandler(event) {
     /* Do something to handler the event here */
     console.log("cancel",event.type);
 
+    if (event.stopPropagation) event.stopPropagation();	// DOM model
+    else event.cancelBubble = true;			// IE model
+
     // Now cancel the default action associated with the event
     if (event.preventDefault) event.preventDefault();	// Standard technique
-    if (event.returnValue) event.returnValue = false;	// IE
+    else event.returnValue = false;			// IE
+
     return false;	// For handlers registered as object properties
 }
 
